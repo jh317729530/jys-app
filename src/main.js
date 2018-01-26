e8c45cded1b96ed88fba549349ef1b966be8deb4
ganjunhui <junhui_g@gzleihou.cn>
2018-01-26 16:13:47 +0800

    基础

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import 'normalize.css'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import router from './router'
+import '@/icons'
+
+import App from './App.vue'
+
+Vue.use(ElementUI)
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
