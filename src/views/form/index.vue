ac5e952bf6dca1e992d6d494a0003dea53a19f93
ganjunhui <317729530@qq.com>
2018-02-07 22:48:33 +0800

    测试页面

---
+++ src/views/form/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app-container">
+      <el-form ref="form" :model="form" label-width="120px">
+          <el-form-item label="Test">
+              <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item>
+              <el-button type="primary" @click="onSubmit">create</el-button>
+              <el-button>cancel</el-button>
+          </el-form-item>
+      </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$message('submit!')
+    }
+  }
+}
+</script>
--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,12 +1,30 @@
 <template>
-  <div>
-      hello world~
+  <div class="test-container">
+      <el-col :span="11"><el-button type="primary" style="width:100%;" @click.native.prevent="test">测试</el-button></el-col>
   </div>
 </template>
 
 <script>
-// export default {
-//   name: 'test'
-// }
+import axios from '../../api/common'
+export default {
+  name: 'test',
+  methods: {
+    test() {
+      axios.get('/api/test/test')
+    }
+  }
+}
 </script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.test {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
 
